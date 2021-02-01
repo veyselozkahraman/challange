d93091ea420499069ed87fdf06a0f76844183b2a
v. alperen ozkahraman <v.alperenozkahraman@vs-MacBook-Pro.local>
2021-02-02 00:27:01 +0300

    implemented product fetching

--- server/server.js
+++ server/server.js
@@ -5,14 +5,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const fetch = require("node-fetch");
 
-// Constants
 const API_KEY = "c873d395f841c64634c5330ce9118c9d";
 const PASSWORD = "5dcf9317c6ff86ea61d21b0d4dd0a1a8";
-const API_URL = `https://${API_KEY}:${PASSWORD}@toolio-retail.myshopify.com/admin/api/2019-10/products.json`
+const API_URL = `https://${API_KEY}:${PASSWORD}@toolio-retail.myshopify.com/admin/api/2019-10/products.json?fields=id,title`
 const PORT = 8081;
 const HOST = '0.0.0.0';
 
-// App
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,18 +21,11 @@ app.get('/', (req, res) => {
   res.send(`Server is listening on port ${PORT}`)
 });
 
-app.get('/products', async (req, res) => {
+app.get('/products', (req, res) => {
   try {
-    const products = [{
-      id: '1',
-      title: 'some title with AWESOME'
-    },
-    {
-      id: '2',
-      title: 'some other title'
-    }
-    ];
-    res.send(products);
+    fetch(API_URL).then(response => response.json()).then(productsObject => {
+      res.send(productsObject.products);
+    });
   } catch (error) {
     console.log(error);
     res.send({});
